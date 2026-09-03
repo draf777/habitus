9323a6e81bd146b0c853c30e0fcf1ee1b065ef21
Roth David <david.roth@santiskurs.ch>
2026-09-03 15:13:19 +0200

    capacitor entfernt

--- projekt/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ch.davidroth.habitus',
-  appName: 'Habitus',
-  webDir: 'www'
-};
-
-export default config;
